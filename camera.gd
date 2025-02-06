b73eb450b0a83cc57a61349c872df83a733d2bdb
Mika Viljanen <mika.viljanen@wunderdog.fi>
2025-02-06 18:05:10 +0200

    Add rotation of the camera

--- camera.gd
+++ camera.gd
@@ -3,5 +3,9 @@ extends Camera3D
 @export var target : Node3D;
 
 func _process(delta):
-	position = position.rotated(Vector3.UP, delta)
+	if Input.is_action_pressed("ui_camera_rotate_cw"):
+		position = position.rotated(Vector3.UP, -delta);
+	if Input.is_action_pressed("ui_camera_rotate_ccw"):
+		position = position.rotated(Vector3.UP, delta);
+
 	look_at(target.position)
